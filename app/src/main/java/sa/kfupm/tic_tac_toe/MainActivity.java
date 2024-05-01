c564a4ac76aeb67b3421d2ef7e05e027029013f9
Feras532 <saf_523_@hotmail.com>
2024-05-01 14:30:29 +0300

    first change

---
+++ app/src/main/java/sa/kfupm/tic_tac_toe/MainActivity.java
@@ -0,0 +1,15 @@
+package sa.kfupm.tic_tac_toe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // 9lo 3la alnabbi mohammed
+    }
+}
